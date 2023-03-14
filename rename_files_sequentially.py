e385d202695239c45eb4349ffc4ba3413b384512
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-03-15 01:44:50 +0800

    Update rename_files_sequentially.py

--- rename_files_sequentially.py
+++ rename_files_sequentially.py
@@ -4,4 +4,4 @@ _ext = ".jpg"
 for i,filename in enumerate(os.listdir(_src)):
     filename = filename.lower()
     if filename.endswith(_ext):
-        os.rename(filename, _src+'opening' + str(i).zfill(3)+_ext)
+        os.rename(filename, _src + 'opening - ' + str(i).zfill(3)+_ext)
