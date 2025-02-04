ab170a20b8f41fb6a894cdf81cfec098841f9fcc
Lee <48121658+lee2nd@users.noreply.github.com>
2025-02-05 01:46:34 +0800

    Update rename_hierarchy_folder_filename.py

--- rename_hierarchy_folder_filename.py
+++ rename_hierarchy_folder_filename.py
@@ -3,9 +3,9 @@ import glob
 
 path_lst = glob.glob(r"檔案路徑")
 
-for floder in path_lst:
-    print(floder)
-    for i,filename in enumerate(os.listdir(floder)):
+for folder in path_lst:
+    print(folder)
+    for i,filename in enumerate(os.listdir(folder)):
         path = floder+"\\"+filename
         if path.endswith("JPG") or path.endswith("jpg"):
             os.rename(path, "\\".join(path.split("\\")[:-1])+"\\"+str(i).zfill(3)+".jpg")
